daa44f2fcbba3b6def431fd21cb09469a389f52b
Yasin Azimi <yasin_azimi@live.com>
2016-05-17 12:11:23 +1000

    bug fixes

--- studentlist.c
+++ studentlist.c
@@ -1,23 +1,22 @@
-/**********************************************************************
+/***************************************************************************
 48430 Embedded C - Assignment 2
 Name: Yasin Azimi
 Student ID: 11733490
-Date of submission: 16/05/2016
+Date of submission: 17/05/2016
 A brief statement on what you could achieve (less than 50 words):
 = 
 
 A brief statement on what you could NOT achieve (less than 50 words):
 = 
 
-**********************************************************************/
+***************************************************************************/
 
-/**********************************************************************
+/**************************************************************************
 List of header files and preprocessing directives
-**********************************************************************/
+***************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 
 #define MAX_CLASS_SIZE 5
 #define MAX_NAME_SIZE 11
@@ -25,10 +24,9 @@ List of header files and preprocessing directives
 
 
 
-/**********************************************************************
-
-**********************************************************************/
-
+/**************************************************************************
+Structs
+***************************************************************************/
 struct date {
 	int day;
 	int month;
@@ -45,81 +43,67 @@ typedef struct student studentcount;
 
 
 
-/**********************************************************************
+/**************************************************************************
 Function prototypes
-**********************************************************************/
+***************************************************************************/
+int flush_stdin();
 void printmenu(void);
 void addstudent(studentcount count[], int* studentsize);
+void deletestudent(studentcount count[], int* studentsize);
 void showstudentlist(studentcount count[], int* studentsize);
 void savestudentlist(studentcount count[], int* studentsize);
 void readstudentlist(studentcount count[], int* studentsize);
-void flush_stdin(void);
 
 
 
-/**********************************************************************
+/**************************************************************************
 Validating integer values
-**********************************************************************/
-char checkRange(char max, char min, char num) {
-    if (num <= max && num >= min) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
+***************************************************************************/
+int flush_stdin() {
+	int c;
+	while ((c = getchar()) != '\n' && c != EOF);
+	return 1;
 }
 
 
 
-/**********************************************************************
-Validating integer values
-**********************************************************************/
-void flush_stdin(void) {
-  int c;
-  while ((c = getchar()) != '\n' && c != EOF);
-}
-
-
-
-/**********************************************************************
+/**************************************************************************
 Main
-**********************************************************************/
-int main(void){
-	studentcount studentlist[MAX_CLASS_SIZE-1];
-	char SENTINEL = NULL;
+***************************************************************************/
+int main(void) {
+	studentcount studentlist[MAX_CLASS_SIZE];
+	int SENTINEL = 0;
 	int studentsize = 0;
 	char option;
 
-	while (option != '6') {
+	do {
 		printmenu();
 		printf("Enter your choice>");
 		scanf(" %c", &option);
 		switch (option)
 		{
 			case '1':
-					if (SENTINEL == 0) {
-						if (studentsize >= 0 && studentsize <= 4) {
-							addstudent(studentlist, &studentsize);
-						}
-					}
-					else if (SENTINEL != 1) {
-						if (studentsize > 4) {
-							printf("Class is full\n");
-						}
+					if (studentsize < MAX_CLASS_SIZE) {
+						addstudent(studentlist, &studentsize);
+					} else {
+						printf("Class is full\n");
 					}
 					break;
 					
 			case '2':
 					if (studentsize > 0) {
-						studentsize--;
-					}
-					else if (studentsize <= 0) {
+						deletestudent(studentlist, &studentsize);
+					} else {
 						printf("Class is empty\n");
 					}
 					break;
 					
 			case '3':
-					showstudentlist(studentlist, &studentsize);
+					if (studentsize <= 0) {
+						printf("Class is empty\n");
+					} else {
+						showstudentlist(studentlist, &studentsize);
+					}
 					break;
 
 			case '4':
@@ -127,26 +111,31 @@ int main(void){
 					break;
 					
 			case '5':
-					readstudentlist(studentlist, &studentsize);
+					if (SENTINEL != 1) {
+						printf("Read error\n");
+					} else {
+						readstudentlist(studentlist, &studentsize);
+					}
 					break;
 					
 			case '6':
 					exit(EXIT_SUCCESS);
+					break;
 					
 			default:
 					printf("Invalid choice.\n");
 					break;
 		}
     }
-	
+	while (option != '6' && flush_stdin());
     return 1;
 }
 
 
 
-/**********************************************************************
-Choices menu
-**********************************************************************/
+/**************************************************************************
+Menu
+***************************************************************************/
 void printmenu(void){
     printf("\n\n"
                    "1. add student\n"
@@ -159,9 +148,9 @@ void printmenu(void){
 
 
 
-/**********************************************************************
-Add new student
-**********************************************************************/
+/**************************************************************************
+Add student
+***************************************************************************/
 void addstudent(studentcount count[], int* studentsize) {
 	studentcount addnewstudent;
 	printf("Enter name>");
@@ -211,11 +200,20 @@ void addstudent(studentcount count[], int* studentsize) {
 
 
 
-/**********************************************************************
-Display the students in a list
+/**************************************************************************
+Delete last student
+***************************************************************************/
+void deletestudent(studentcount count[], int* studentsize) {
+	if (*studentsize > 0) {
+		(*studentsize)--;
+	}
+}
 
-// printf("Class is empty\n");
-**********************************************************************/
+
+
+/**************************************************************************
+Display student list
+***************************************************************************/
 void showstudentlist(studentcount count[], int* studentsize) {
 	int element;
 	printf("Name       Birthday   GPA\n");
@@ -229,41 +227,40 @@ void showstudentlist(studentcount count[], int* studentsize) {
 
 
 
-/**********************************************************************
-Save studentlist to database
-
-// overwrite if database file already exists
-**********************************************************************/
+/**************************************************************************
+Save the student list to the database
+***************************************************************************/
 void savestudentlist(studentcount count[], int* studentsize) {
 	int record;
 	FILE *writefile;
-	writefile = fopen("database", "w");
+	writefile = fopen(DB_FILE_NAME, "w");
 	for (record = 0; record < *studentsize; record++) {
-		fprintf(writefile, "%s\t", count[record].name);
-		fprintf(writefile, "%d\t%d\t%d\t", count[record].birthday.day, count[record].birthday.month, count[record].birthday.year);
-		fprintf(writefile, "%.6f\n", count[record].gpa);
+		fprintf(writefile, "%11s ", count[record].name);
+		fprintf(writefile, "%02d-%02d-%d ", count[record].birthday.day, count[record].birthday.month, count[record].birthday.year);
+		fprintf(writefile, "%f\n", count[record].gpa);
 	}
 	fclose(writefile);
 }
 
 
 
-/**********************************************************************
-Read studentlist from database
-**********************************************************************/
+/**************************************************************************
+Read the student list from the database
+***************************************************************************/
 void readstudentlist(studentcount count[], int* studentsize) {
-	int r;
+	int r = 0;
 	FILE *readfile;
-	
-	readfile = fopen("database", "r");
-	if (readfile == NULL) {
-		printf("Read error\n");
+	readfile = fopen(DB_FILE_NAME, "r");
+	while (r < MAX_CLASS_SIZE) {
+		if (fscanf(readfile, "%11s ", count[r].name)) {
+			r++; }
+		if (fscanf(readfile, "%02d-%02d-%d ", &count[r].birthday.day, &count[r].birthday.month, &count[r].birthday.year)) {
+			r++; }
+		if (fscanf(readfile, "%f\n", &count[r].gpa)) {
+			r++; }
 	}
-	else if (readfile) {
-		while ((r = getc(readfile)) != EOF)
-			putchar(r);
-		fclose(readfile);
-	}	
+	*studentsize = r;
+	fclose(readfile);
 }
 
 
